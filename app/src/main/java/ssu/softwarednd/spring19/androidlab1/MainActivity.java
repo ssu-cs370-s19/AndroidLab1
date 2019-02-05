820ae94d9b2b1ecd6913cfaf166fab35164d9129
Tyler Holland <trollyhandle@gmail.com>
2019-02-04 18:33:29 -0800

    starter code

---
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab1/MainActivity.java
@@ -0,0 +1,16 @@
+package ssu.softwarednd.spring19.androidlab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  // set layout for this Activity
+
+
+    }
+}
