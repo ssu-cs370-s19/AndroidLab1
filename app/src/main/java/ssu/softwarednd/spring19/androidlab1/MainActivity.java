daba4a76bc1444a63d227e5ee06cba6afdd38179
student <student@ssu-cs-dar28-167-223.sonoma.edu>
2019-02-11 19:19:08 -0800

    lab 1: working code complete

--- app/src/main/java/ssu/softwarednd/spring19/androidlab1/MainActivity.java
+++ app/src/main/java/ssu/softwarednd/spring19/androidlab1/MainActivity.java
@@ -2,14 +2,27 @@ package ssu.softwarednd.spring19.androidlab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button nameButton;
+private TextView nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);  // set layout for this Activity
+        nameText = findViewById(R.id.name_text);
+        nameButton = findViewById(R.id.name_button);
+
+        nameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                nameText.setText(R.string.name_text);
+            }
+        });
 
 
     }
